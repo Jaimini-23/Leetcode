61c9c680df96db0d7b83a965fc46bb534140c87d
Jaimini-23 <mayankjaimini23@gmail.com>
2026-07-01 20:38:22 +0530

    Time: 3 ms (46.86%), Space: 25 MB (89.05%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -2,21 +2,45 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans;
-        long long S = 0, S2 = 0;
-        long long Sn = (1LL * n * (n + 1)) / 2;
-        long long S2n = (1LL * n * (n + 1) * (2 * n + 1)) / 6;
-        for(int i=0;i<n;i++) {
-            S += nums[i];
-            S2 += (long long)nums[i] * (long long)nums[i];
+        int xr = 0;
+
+        // XOR of array elements and numbers from 1 to n
+        for (int i = 0; i < n; i++) {
+            xr ^= nums[i];
+            xr ^= (i + 1);
         }
-        long long val1 = S - Sn;           // x - y
-        long long val2 = S2 - S2n;        // x^2 -y^2
-        val2 = val2 / val1;                // x + y
-        long long x = (val1 + val2) / 2;
-        ans.push_back(x);
-        long long y = x - val1;
-        ans.push_back(y);
-        return ans;
+
+        // Rightmost set bit
+        int number = xr & ~(xr - 1);
+
+        int one = 0, zero = 0;
+
+        // Divide array elements into two groups
+        for (int i = 0; i < n; i++) {
+            if (nums[i] & number)
+                one ^= nums[i];
+            else
+                zero ^= nums[i];
+        }
+
+        // Divide numbers 1 to n into two groups
+        for (int i = 1; i <= n; i++) {
+            if (i & number)
+                one ^= i;
+            else
+                zero ^= i;
+        }
+
+        // Determine which is repeating
+        int cnt = 0;
+        for (int x : nums) {
+            if (x == one)
+                cnt++;
+        }
+
+        if (cnt == 2)
+            return {one, zero};   // repeating, missing
+        else
+            return {zero, one};   // repeating, missing
     }
 };
